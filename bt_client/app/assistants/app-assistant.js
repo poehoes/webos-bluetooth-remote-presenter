8250ded98f59962f0b3bee2831386c66f71eb767
Ralf Henschkowski <ralf@henschkowski.com>
2012-03-19 21:40:58 +0100

    fixed alarm behaviour after app was closed, removed unused preference option, removed unnecessary logging code

--- bt_client/app/assistants/app-assistant.js
+++ bt_client/app/assistants/app-assistant.js
@@ -7,9 +7,16 @@ AppAssistant.prototype.handleLaunch = function(params) {
     if (!params || (params["message"] == undefined)){
 	return;
     }
+    Mojo.Log.error("Alarm received.");
+
     // Get the default stage controller
-    var stage_controller = this.controller.getStageController("");
-    
+    var stage_controller = this.controller.getStageProxy("");
+    if (stage_controller == undefined) {
+	Mojo.Log.error("App is closed - alarm ignored.");
+	return;
+    }
+	
+    Mojo.Log.error("Alarm activated");
     // Run the function keepAliveTimer to send a keepalive message
     stage_controller.delegateToSceneAssistant("keepAliveTimer");
 };
